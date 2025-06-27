2c0db63edeb8afc086ab993034fbd525d2b1fb62
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-06-27 16:01:52 +0530

    Firt phase v1.4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3  gap-4 p-4">
       {/* Main Overview - spans 2 columns on larger screens */}
       <div className="bg-primary-foreground p-4 rounded-lg sm:col-span-2 2xl:col-span-2">
        <DailyOrders/>
@@ -19,19 +19,19 @@ const Home = () => {
        <RevenueLineChart/>
       </div>
 
-      <div className="bg-primary-foreground  rounded-lg xl:col-span-1 2xl:col-span-2">
+      <div className="bg-primary-foreground  rounded-lg xl:col-span-1 ">
        <RevenuePieChart/>
       </div>
 
       {/* Recent Activity - spans 2 columns on 2XL */}
-      <div className="bg-primary-foreground p-4 rounded-lg xl:col-span-2 2xl:col-span-2">
+      <div className="bg-primary-foreground p-4 rounded-lg xl:col-span-2 ">
        <AreaChartt/>
       </div>
      
 
       {/* More Info or Placeholder Card */}
      
-       <div className="bg-primary-foreground p-4 rounded-lg xl:col-span-2 2xl:col-span-2">
+       <div className="bg-primary-foreground p-4 rounded-lg xl:col-span-2 ">
       <EmployeeCards/>
       </div>
 
